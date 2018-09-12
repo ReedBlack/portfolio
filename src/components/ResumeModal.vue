9674129af4a04179637c62441d7c12d8d8fcb6a8
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-09-12 17:37:48 -0600

    transitions

--- src/components/ResumeModal.vue
+++ src/components/ResumeModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <iframe class="lg-resume"  src="https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/Reed-Black-Resume-7-29.pdf" style="width:100%; height:600px;" frameborder="0"></iframe>
+        <iframe class="lg-resume"  src="https://s3-ap-southeast-1.amazonaws.com/mixtap-mixes/Reed-Black-Resume-7-29.pdf#zoom=100" style="width:100%; height:600px;" frameborder="0"></iframe>
     </div>
 </template>
 
